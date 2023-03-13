6343d9ad39e210f313537ea0ab41d06426a32714
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-03-13 13:55:57 +0400

    WIP

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class TestModel(models.Model):
+    
+    name = models.CharField(_("Hello"), max_length=100)
+    
+    class Meta:
+        verbose_name = _("Test")
+        verbose_name_plural = _("Tests")
