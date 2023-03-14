907308338c524ecfe8f3d2f09ccaae05c6c6e582
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-03-14 19:41:54 +0400

    test commands

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_po_translator',
-    version='2.1.3',
+    version='2.1.4',
     description='Your package description',
     packages=find_packages(),
-     entry_points={
+    entry_points={
         'console_scripts': [
             'translate=django_po_translator.management.commands.translate:Command',
             'restore_po_formatting=django_po_translator.management.commands.restore_po_formatting:Command'
