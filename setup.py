525f2254abde9a832f0140e83fe78a7c7f34503b
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-03-14 18:58:30 +0400

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_po_translator',
-    version='2.0.0',
+    version='2.1.0',
     description='Your package description',
     packages=find_packages(),
     install_requires=[
