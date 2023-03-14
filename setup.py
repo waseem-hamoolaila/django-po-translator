bbe82de071b2cd63dfa7efae1c814a57e95d82ac
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-03-14 19:12:00 +0400

    Fix manifest file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_po_translator',
-    version='2.1.1',
+    version='2.1.2',
     description='Your package description',
     packages=find_packages(),
     install_requires=[
@@ -18,4 +18,5 @@ setup(
     url='https://github.com/waseem-hamoolaila/django-po-translator',
     author='Waseem Hamoolaila',
     author_email='waseem.97.laila@gmail.com',
+    
 )
