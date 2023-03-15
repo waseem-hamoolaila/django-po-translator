30fafa8ab20a4d6314a666fed0a8870697801754
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-03-15 09:54:01 +0400

    enhance tests

--- django_po_translator/tests.py
+++ django_po_translator/tests.py
@@ -57,6 +57,21 @@ class TestPoTranslator(test.TestCase):
           self.assertEqual(lines.count("#, fuzzy\n"), 1) # only the one in the title
           
           
+     def test_line_processor_without_fixing_fuzziness(self):
+          
+          lines = None
+          po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'tests', 'test_po.po'))
+          with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
+               lines = po_file.readlines()
+          
+          self.assertEqual(lines.count("#, fuzzy\n"), 2)
+          res, lines = lines_processor(lines, 'ar', update_already_translated=True, resolve_fuzzy=False)
+          
+          self.assertTrue(res)
+          self.assertNotEqual(len(lines), 0)
+          self.assertEqual(lines.count("#, fuzzy\n"), 2) # only the one in the title
+          
+          
      def test_line_processor_without_override_translations(self):
           
           lines = None
