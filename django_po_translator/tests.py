961f5691c6d4585ec194a7ecf609460b982ddd86
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-03-21 12:49:26 +0400

    New tests

--- django_po_translator/tests.py
+++ django_po_translator/tests.py
@@ -1,99 +1,38 @@
-# from django import test
-# import os 
+from django import test
+import os
 
-# from django_po_translator.translate import translate_text
-# from django_po_translator.processors import lines_processor, action
+from django_po_translator.po_processor import PoProcessor
 
-
-# class TestPoTranslator(test.TestCase):
-                
-#      def test_translate_ep(self):
-
-#           translated_text = translate_text(text="Hello",target_language="ar")
-#           self.assertIsNotNone(translated_text)
-          
-          
-#      def test_action_processor(self):
-#           lines = None
-#           po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
-#           with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
-#                lines = po_file.readlines()
-               
-#           self.assertIsNotNone(lines)
-          
-          
-#      def test_line_processor(self):
-          
-#           lines = None
-#           po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
-#           with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
-#                lines = po_file.readlines()
-               
-#           res, lines = lines_processor(lines, 'ar', update_already_translated=True, resolve_fuzzy=True)
-          
-#           self.assertTrue(res)
-#           self.assertNotEqual(len(lines), 0)
-          
-          
-#      def test_line_processor_after_fixing_fuzziness(self):
-          
-#           lines = None
-#           po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
-#           with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
-#                lines = po_file.readlines()
-          
-#           self.assertEqual(lines.count("#, fuzzy\n"), 2)
-#           res, lines = lines_processor(lines, 'ar', update_already_translated=True, resolve_fuzzy=True)
-          
-#           self.assertTrue(res)
-#           self.assertNotEqual(len(lines), 0)
-#           self.assertEqual(lines.count("#, fuzzy\n"), 1) # only the one in the title
-          
-          
-#      def test_line_processor_without_fixing_fuzziness(self):
-          
-#           lines = None
-#           po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
-#           with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
-#                lines = po_file.readlines()
-          
-#           self.assertEqual(lines.count("#, fuzzy\n"), 2)
-#           res, lines = lines_processor(lines, 'ar', update_already_translated=True, resolve_fuzzy=False)
-          
-#           self.assertTrue(res)
-#           self.assertNotEqual(len(lines), 0)
-#           self.assertEqual(lines.count("#, fuzzy\n"), 2) # only the one in the title
-          
-          
-#      def test_line_processor_without_override_translations(self):
-          
-#           lines = None
-#           po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
-#           with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
-#                lines = po_file.readlines()
-          
-#           # This is a wrong translation .. added intentionally
-#           self.assertEqual(lines.count('msgstr "صحيح"\n'), 1)
-            
-#           res, lines = lines_processor(lines, 'ar', update_already_translated=False, resolve_fuzzy=True)
-          
-#           # wrong translation should be stick in there .. without any change
-#           self.assertEqual(lines.count('msgstr "صحيح"\n'), 1)
+class TestPoProcessor(test.TestCase):
+    
+    def setUp(self):
         
-     
-#      def test_line_processor_with_override_translations(self):
-          
-#           lines = None
-#           po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
-#           with open(po_test_file_path, 'r', encoding='utf-8') as po_file:
-#                lines = po_file.readlines()
-          
-#           # This is a wrong translation .. added intentionally
-#           self.assertEqual(lines.count('msgstr "صحيح"\n'), 1)
-            
-#           res, lines = lines_processor(lines, 'ar', update_already_translated=True, resolve_fuzzy=True)
-          
-#           # wrong translation should be overridden
-#           self.assertEqual(lines.count('msgstr "صحيح"\n'), 0)
-     
-     
+        self.po_test_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_files', 'test_po.po'))
+    
+    def test_read_po_file(self):
+        
+        po_processor = PoProcessor(po_file_path=self.po_test_file_path, target_language='ar')
+        self.assertNotEqual(po_processor.get_po_files_entries(), [])
+        
+    def test_initial_resolve_fuzzy(self):
+        
+        po_processor = PoProcessor(po_file_path=self.po_test_file_path, target_language='ar')
+        original_po_entries = po_processor.get_po_files_entries()
+        
+        self.assertTrue(po_processor.initial_resolve_fuzziness())
+        self.assertNotIn("#, fuzzy\n", po_processor.get_po_files_entries())
+        
+        # restore original state    
+        po_processor.update_po_dir(processed_entries=original_po_entries)
+        
+    
+    def test_translate_po(self):
+        
+        po_processor = PoProcessor(po_file_path=self.po_test_file_path, target_language='ar')
+        original_po_entries = po_processor.get_po_files_entries()
+        
+        self.assertTrue(po_processor.initial_translation_process(all=True))
+        self.assertNotIn("msgstr ""\n", po_processor.get_po_files_entries())
+        
+        # restore original state    
+        po_processor.update_po_dir(processed_entries=original_po_entries)
